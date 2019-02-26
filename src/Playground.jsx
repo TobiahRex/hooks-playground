a64594fc26d8baaee5a8eed07cc45fec9383adb9
TobiahRex <tobiah.rex@gmail.com>
2019-02-26 09:00:06 -0800

    COMPLETED / Boilerplate  --------------------------------------------------------------------   MAIN   * Made a new Playground.js files that allows us to start displaying     a list of names using Hooks & Classes for comparison.  --------------------------------------------------------------------   OTHER   * Style sheet updates.  --------------------------------------------------------------------

--- src/Playground.jsx
+++ src/Playground.jsx
@@ -6,15 +6,29 @@ export default class Playground extends React.Component {
 
     this.state = {
       name: [''],
+      names: [],
     };
   }
 
   handleOnChange = (e) => {
     const { target } = e;
+
     this.setState((prevState) => {
-      prevState.name.push(target.value.slice(-1));
+      prevState.name.length = 0;
+      prevState.name.push(target.value);
       return ({
         name: prevState.name,
+      });
+    });
+  }
+
+  handleSubmitName = () => {
+    this.setState((state) => {
+      state.names.push(state.name.join(''));
+      state.name.length = 0;
+      return ({
+        names: state.names,
+        name: state.name,
       });
     });
   }
@@ -22,19 +36,58 @@ export default class Playground extends React.Component {
   render() {
     return (
       <div>
-        <input
-          placeholder={'Enter a name...'}
-          value={this.state.name.join('')}
-          onChange={this.handleOnChange}
+        <div>
+          <input
+            placeholder={'Enter a name...'}
+            value={this.state.name.join('')}
+            onChange={this.handleOnChange}
 
-          style={{
-            marginTop: '30vh',
-            width: '400px',
-            height: '30px',
-            fontSize: '14px',
-          }}
-        />
+            style={{
+              paddingLeft: 5,
+              border: 'none',
+              marginTop: '30vh',
+              width: '400px',
+              height: '30px',
+              fontSize: '14px',
+            }}
+          />
+          <button
+            style={{
+              marginLeft: 5,
+              width: '100px',
+              height: '33px',
+              fontSize: '14px',
+              border: 'none',
+            }}
+            type="button"
+            onClick={this.handleSubmitName}>
+            Submit
+          </button>
+        </div>
+        <div>
+          <ul>
+            {
+              this.state.names.map((name) => (
+                <li
+                  key={name}
+                  style={{
+                    marginTop: 5,
+                  }}>
+                  {name}
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+
       </div>
     )
   }
 }
+
+/*
+  NOTE: Materials
+    https://codesandbox.io/embed/github/elijahmanor/egghead-course-react-hooks/tree/omega/02-add-state-to-a-react-function-component-with-the-useState-hook/react-hooks
+  NOTE: Course
+    https://egghead.io/lessons/react-add-state-with-the-usestate-hook-to-a-react-function-component
+*/
